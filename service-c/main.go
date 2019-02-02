6562f0ef23167b3b4b3a4c56d0aaecfd9f17a628
Gary A. Stafford <garystafford@rochester.rr.com>
2019-02-02 16:57:19 -0500

    Adding Dockerfiles

---
+++ service-c/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+	"github.com/mongodb/mongo-go-driver/mongo"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+type Trace struct {
+	ID          string    `json:"id,omitempty"`
+	ServiceName string    `json:"serviceName,omitempty"`
+	CreatedAt   time.Time `json:"createdAt,omitempty"`
+}
+
+var traces []Trace
+
+func Orchestrator(w http.ResponseWriter, r *http.Request) {
+	//time.Sleep(2000 * time.Millisecond)
+
+	traces = nil
+
+	tmpTrace := Trace{ID: uuid.New().String(), ServiceName: "Service-C", CreatedAt: time.Now().Local()}
+
+	traces = append(traces, tmpTrace)
+	fmt.Println(traces)
+
+	CallMongoDB(tmpTrace)
+
+	json.NewEncoder(w).Encode(traces)
+}
+
+func CallNextService(url string) {
+	var tmpTraces []Trace
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("The HTTP request failed with error %s\n", err)
+	} else {
+		data, _ := ioutil.ReadAll(response.Body)
+		err := json.Unmarshal(data, &tmpTraces)
+		if err != nil {
+			panic(err)
+		}
+
+		for _, r := range tmpTraces {
+			traces = append(traces, r)
+		}
+	}
+}
+
+func CallMongoDB(trace Trace) {
+	//print(os.Getenv("MONGO_CONN"))
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, os.Getenv("MONGO_CONN"))
+	if err != nil {
+		panic(err)
+	}
+
+	collection := client.Database("service-c").Collection("traces")
+	ctx, _ = context.WithTimeout(context.Background(), 5*time.Second)
+
+	_, err = collection.InsertOne(ctx, trace)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/ping", Orchestrator).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
