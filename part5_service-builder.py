87ee362fc9ce91342ced0b5b77ac26bbc4678085
Gary A. Stafford <garystafford@rochester.rr.com>
2019-03-10 13:00:39 -0400

    Refactor Jinja template

--- part5_service-builder.py
+++ part5_service-builder.py
@@ -3,7 +3,7 @@
 # author: Gary A. Stafford
 # site: https://programmaticponderings.com
 # license: MIT License
-# purpose: Optional: (Re)build (7) Go-based microservice's Kubernetes
+# purpose: Optional: (Re)build (8) Go-based microservice's Kubernetes
 #          Service and Deployment resources from Jinja2 template
 
 from jinja2 import Environment, FileSystemLoader
@@ -18,11 +18,11 @@ services = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 for service in services:
     output = template.render(service=service,
                              replicas=2,
-                             versions='1.0.0')
+                             versions='1.1.0')
     print(output)
 
     filename = "service-%s%s" % (service, '.yaml')
     resource = "%s/%s" % (resource_location, filename)
-    
+
     with open(resource, "w") as f:
         f.write(output)
