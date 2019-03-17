cfd69c003930923760792b55020365c946ef32d1
Gary A. Stafford <garystafford@rochester.rr.com>
2019-03-17 09:00:07 -0400

    Fixing missing header

--- service-a/main.go
+++ service-a/main.go
@@ -31,6 +31,7 @@ var traces []Trace
 
 func PingHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	log.Info(r)
 
 	traces = nil
 	CallNextService("http://service-b/api/ping")
