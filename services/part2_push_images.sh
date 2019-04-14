5ca92dbb133a406d076d239bca4a44813819088f
Gary A. Stafford <garystafford@rochester.rr.com>
2019-04-14 14:15:52 -0400

    Refactor for GCP based images

--- services/part2_push_images.sh
+++ services/part2_push_images.sh
@@ -5,15 +5,17 @@
 # license: MIT License
 # purpose: Push images to Dockerhub
 
-# readonly -a arr=(a b c d e f g h rev-proxy)
-readonly -a arr=(e)
+readonly -a arr=(a b c d e f g h rev-proxy)
+#readonly -a arr=(e)
 readonly tag=1.5.0
 
-for i in "${arr[@]}"
-do
-  docker push "garystafford/go-srv-$i:$tag"
-#  docker tag garystafford/go-srv-$i:$tag gcr.io/go-srv-demo/go-srv-$i:$tag
-#  docker push gcr.io/go-srv-demo/go-srv-$i:$tag
-done
+#for i in ${arr[@]}
+#do
+#  docker push garystafford/go-srv-$i:$tag
+#  docker tag garystafford/go-srv-${i}:${tag} gcr.io/go-srv-demo/go-srv-${i}:${tag}
+#  docker push gcr.io/go-srv-demo/go-srv-${i}:${tag}
+#done
 
-# docker push "garystafford/angular-observe:$tag"
+docker push garystafford/angular-observe:$tag
+docker tag garystafford/angular-observe:${tag} gcr.io/go-srv-demo/angular-observe:${tag}
+docker push gcr.io/go-srv-demo/angular-observe:${tag}
