82edc473e6cccf9409a5ba195e7efef346317e44
Gary A. Stafford <gary.a.stafford@gmail.com>
2021-07-07 11:30:59 -0400

    Add GitHub Action

---
+++ services/part0_build_services_locally.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# author: Gary A. Stafford
+# site: https://programmaticponderings.com
+# license: MIT License
+# purpose: Build Go microservices for demo
+# date: 2021-07-07
+
+readonly -a arr1=(a b c d e f g h)
+for i in "${arr1[@]}"
+do
+  pushd "json-rest/service-$i" || exit
+  rm -rf go.mod
+  rm -rf go.sum
+  go mod init "github.com/garystafford/go-srv-$i"
+  go mod tidy -v
+  go build -ldflags '-w -s' -a -installsuffix cgo -o ./ .
+  popd || exit
+done
+
+readonly -a arr2=(a-grpc b-grpc c-grpc d-grpc e-grpc f-grpc g-grpc h-grpc rev-proxy-grpc)
+for i in "${arr2[@]}"
+do
+  pushd "protobuf-grpc/service-$i" || exit
+  rm -rf go.mod
+  rm -rf go.sum
+  go mod init "github.com/garystafford/go-srv-$i"
+  go mod tidy -v
+  go build -ldflags '-w -s' -a -installsuffix cgo -o ./ .
+  popd || exit
+done
